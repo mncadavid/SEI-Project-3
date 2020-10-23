bf07f817b4fe61fe2df5cb002004f10ce7f00724
lagerhausenandrew <lagerhausen.3@gmail.com>
2020-10-23 14:11:15 -0500

    Started Trip Container

---
+++ project3/src/components/TripContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SelectedPlaceCards from './SelectedPlaceCards';
+
+const TripContainer = () => {
+    return(
+        <div>
+            <SelectedPlaceCards />
+        </div>
+    )
+}
+
+export default TripContainer;
