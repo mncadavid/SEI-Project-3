559659dd3ccee49109563109854d98d199b1a36c
lagerhausenandrew <lagerhausen.3@gmail.com>
2020-10-23 14:25:50 -0500

    Called trip container to app

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import './App.css';
+import TripContainer from './components/TripContainer'
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <TripContainer />
       </div>
     );
   }
