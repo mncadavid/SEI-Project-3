11269ec743806acf54f322c8bd64a24e8257808c
Andrew Martin <martinandrew2@johndeere.com>
2020-10-23 16:21:24 -0500

    Added search bar with functionality and passing search results back to app state

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,21 +1,30 @@
 import React,{Component} from 'react';
 import './App.css';
+import {Route,Link} from 'react-router-dom';
 
 import SearchBar from './components/SearchBar';
+import ResultsPage from './components/ResultsPage';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      searchResults: []
     }
+  }
+
+  updateSearchResults = (data) => {
+    this.setState({
+      searchResults: data
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <SearchBar />
+        <Route exact path='/' render={()=><SearchBar updateSearchResults={this.updateSearchResults} />} />
+        <Route exact path='/results' render={()=><ResultsPage results={this.state.searchResults} />} />
       </div>
     );
   }
--- project3/src/components/SearchBar.jsx
+++ project3/src/components/SearchBar.jsx
@@ -1,44 +1,67 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Script from 'react-load-script';
+import {Link} from 'react-router-dom';
 
 class SearchBar extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            searchCriteria: '',
-            autocompleteOptions: []
+            nearbySearchResults: []
         }
     }
 
-    selectAutocompleteOption = () => {
+    handleScriptLoad = () => {
+        /*global google*/
+        const autocomplete = new google.maps.places.Autocomplete(document.querySelector('#searchBox'));
 
+        const map = new google.maps.Map(
+            document.querySelector('#map'), 
+            {
+                center: {
+                    lat: 38.478320,
+                    lng: -97.866323
+                }, 
+                zoom: 4
+            }
+        );
+
+        autocomplete.addListener('place_changed',()=>{
+            const place = autocomplete.getPlace();
+
+            const options = {
+                radius: 10000, 
+                location: new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng()),
+                type: ['restaurant']
+            }
+
+            new google.maps.places.PlacesService(map).nearbySearch(options, this.searchResults)            
+        })
     }
 
-    updateSearchCriteria = async (e) => {
-        const response = await axios.get()
-        console.log(response)
-        this.setState({searchCriteria: e.target.value})
+    searchResults = (results,status) => {
+        this.props.updateSearchResults(results);
     }
 
     render() {
         return (
             <div>
+                <Script 
+                    url={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&libraries=places`}
+                    onLoad={this.handleScriptLoad}
+                />
                 <h2>Where do you wanna go?</h2>
                 <form>
                     <input 
                         type='text' 
+                        id='searchBox'
                         name='searchCriteria' 
                         placeholder='Search' 
-                        onChange={(e)=>this.updateSearchCriteria(e)}
                         value={this.state.searchCriteria}
                     />
-                    <input 
-                        type='submit'
-                        value='Search'
-                    />
                 </form>
-                <div>
+                <Link to='/results'>Search</Link>
+                <div id='map'>
 
                 </div>
             </div>
