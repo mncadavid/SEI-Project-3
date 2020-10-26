540ecee465a9f4e5b92b5e844030065d54de155e
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-10-26 11:26:14 -0500

    Updated Results Container and PlaceCard

--- project3/src/components/PlaceCard.jsx
+++ project3/src/components/PlaceCard.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import Card from '@material-ui/core/Card';
 
 function PlaceCard(props){
     let dollars = new Array(props.place.price_level);
@@ -7,17 +10,17 @@ function PlaceCard(props){
     }
 
     return(
-        <div className="place-card">
-            <button onClick={() => props.handleAddToTrip(props.place)}>Add To Trip</button>
+        <Card variant="outlined" className="place-card">
+            <Button variant="contained" color="primary" onClick={() => props.handleAddToTrip(props.place)}>Add To Trip</Button>
             <h3 className="rating">&#9733;{props.place.rating}</h3>
             <div className="place-card-inner">
                 <h4 className="place-name">{props.place.name}</h4>
-                {dollars.forEach(dollar => <p key={dollar}>$</p>)}
+                
+                <Icon>attach_money</Icon>
             </div>
-            <p className="description">{props.place.description}</p>
-            <img className="learn-more" src="https://upload.wikimedia.org/wikipedia/commons/5/55/Magnifying_glass_icon.svg" alt="Learn More" 
-                onClick={() => props.handleDetailsClick(props.place)}/>
-        </div>
+            <Icon color="primary"
+                onClick={() => props.handleDetailsClick(props.place)}>zoom_in</Icon>
+        </Card>
     )
 }
 
