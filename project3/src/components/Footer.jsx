a3015842de6281d425f0c0eb40c54f33f34caaeb
lagerhausenandrew <lagerhausen.3@gmail.com>
2020-10-28 11:26:36 -0500

    added link to one portfolio

--- project3/src/components/Footer.jsx
+++ project3/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
     return(
         <footer>
                 <p>&#169;2020 (Cool App Name Here)</p>
-                <p>Cadavid -- Martin -- Lagerhausen</p>
+                <p><a href="" target="_blank">Cadavid Berns</a> -- <a href="" target="_blank">Martin</a> -- <a href="http://unsuitable-bat.surge.sh/" target="_blank">Lagerhausen</a></p>
                 <a href="https://github.com/mncadavid/SEI-Project-3/issues">Submit Issue Here!</a>
         </footer>
     )
